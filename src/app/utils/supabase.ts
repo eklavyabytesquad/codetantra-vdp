bb1632af68ac7802c483ccce43bbe7d4e34c91e3
Eklavya Singh <23f2001512@ds.study.iitm.ac.in>
2025-02-06 10:36:35 +0530

    updatedd code

--- src/app/utils/supabase.ts
+++ src/app/utils/supabase.ts
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 // Environment variables for Supabase configuration
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const supabaseUrl = "https://wvlkqoyngzifsjakqjna.supabase.co ";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind2bGtxb3luZ3ppZnNqYWtxam5hIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc5MDkyMDMsImV4cCI6MjA1MzQ4NTIwM30.DcF5E8upAvq0Y5PYLrkL4YuhhBCCMEeGM3shDtYp148";
 // Create a single instance of the Supabase client
 const supabase = createClient(supabaseUrl, supabaseKey);
 
